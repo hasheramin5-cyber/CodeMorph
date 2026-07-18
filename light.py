8d933adcd545f02547da3bf520ae7f32e15727fa
Hasher Amin <hasheramin.5@gmail.com>
2026-07-18 13:11:04 +0500

    Add fullscreen light window with ESC exit

--- light.py
+++ light.py
@@ -1,13 +1,21 @@
+import tkinter as tk
+
+
 def play_morse_light(encoded_message):
-    pass
+    window = tk.Tk()
+    window.title("CodeMorph Signaling")
+    window.configure(bg="white")
+    window.geometry("400x400+300+150")
+    window.attributes("-fullscreen", True)
+    window.mainloop()
   
 def flash_dot():
     pass
 
-
 def flash_dash():
     pass
 
-
 def pause():
     pass
+
+play_morse_light("...")
